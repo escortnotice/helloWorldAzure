2e6481609d9efe0a3c740b85953fe8d758d5eb6f
shreynamdeo <shreynamdeo@gmail.com>
2020-11-06 14:53:42 +0530

    Enabling CORS

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.Repository.UserRepository;
 import com.example.demo.model.BasicUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DemoController {
     @Autowired
     UserRepository userRepository;
